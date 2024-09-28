bef80382e01f7c73a60e9d096408a81762788ee5
lxm <walkking1@163.com>
2024-09-28 18:04:59 +0800

    90%

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -8,10 +8,18 @@
 //
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 trait AppendBar {
     fn append_bar(self) -> Self;
+}
+
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        let mut bar = vec![String::from("Bar")];
+        let mut origin = self.clone();
+        origin.append(&mut bar);
+        origin
+    }
 }
 
 // TODO: Implement trait `AppendBar` for a vector of strings.
